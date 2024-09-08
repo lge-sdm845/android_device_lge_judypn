d8859296aede9e6352b0050475a63bb26bac2f58
Michael Bestas <mkbestas@lineageos.org>
2024-09-08 06:43:03 +0300

    judypn: fix legacy libprotobuf-cpp-full ELF check
    Change-Id: Iadab26630d4599d5df8630d73c8eee254ea2a13f

--- setup-makefiles.sh
+++ setup-makefiles.sh
@@ -5,6 +5,27 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
+function lib_to_package_fixup_vendor_variants() {
+    if [ "$2" != "vendor" ]; then
+        return 1
+    fi
+
+    case "$1" in
+        libprotobuf-cpp-full)
+            echo "libprotobuf-cpp-full-vendorcompat"
+            ;;
+        *)
+            return 1
+            ;;
+    esac
+}
+
+function lib_to_package_fixup() {
+    lib_to_package_fixup_clang_rt_ubsan_standalone "$1" ||
+        lib_to_package_fixup_proto_3_9_1 "$1" ||
+        lib_to_package_fixup_vendor_variants "$@"
+}
+
 # If we're being sourced by the common script that we called,
 # stop right here. No need to go down the rabbit hole.
 if [ "${BASH_SOURCE[0]}" != "${0}" ]; then
