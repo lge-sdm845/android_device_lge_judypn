46eada794089dcd19e603b0e59823a01ab8a49c2
LuK1337 <priv.luk@gmail.com>
2025-01-07 10:47:00 +0100

    judypn: Clear LIBC_PRIVATE symbol versions for libmmcamera_faceproc
    Change-Id: I1565a6fef5070377650e62dc3e3a45bfd2bfaf5b

--- extract-files.py
+++ extract-files.py
@@ -26,7 +26,11 @@ blob_fixups: blob_fixups_user_type = {
         'vendor/lib/liblgsnpeawb.so',
         'vendor/lib/libSNPE.so'
     ): blob_fixup()
-        .replace_needed('libstdc++.so', 'libstdc++_vendor.so')
+        .replace_needed('libstdc++.so', 'libstdc++_vendor.so'),
+    'vendor/lib/libmmcamera_faceproc.so': blob_fixup()
+        .clear_symbol_version('__aeabi_memcpy')
+        .clear_symbol_version('__aeabi_memset')
+        .clear_symbol_version('__gnu_Unwind_Find_exidx'),
 }  # fmt: skip
 
 
